a2a97b5e9ee6ee6b082dc2f0fb99b340f78f86b7
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2022-09-22 18:47:37 +0300

    homework completed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,60 @@
 <script setup>
+import { onMounted } from "vue";
 import BootcampLogo from "./assets/svg/BootcampLogo.vue"
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+import fecthingData from "./assets/scripts/fetchedData";
+import {ref} from "vue"
+import StudentsByGroup from "./components/StudentsByGroup.vue";
+const filteredByAssisstantData = ref([]);
+const groupNames = ref([]) // assigned student values to ref in order to use it later
 
+function getGroupNames (data) {
+	const groups = data.map(element  => element.group);
+	let unique = [...new Set(groups)];
+
+
+	groupNames.value = unique
+	console.log(unique);
+
+	return unique
+}
+function getElementByGroupName(groupName){
+
+	let groupData = filteredByAssisstantData.filter(element => element.group == groupName)
+
+	let newGroupData = groupData.students
+
+	return newGroupData;
+
+}
+function studentsByGroup(data) {  // this function return students as required in order to use them.
+	let asistants = data.filter(student => student.type);
+	let students = data.filter(student => !student.type);
+	asistants = asistants.map(asistant => {
+		const filteredStudents = students.filter(
+			(student) => student.group === asistant.group
+		);
+		let newfiltered = filteredStudents.map(name => name.name)
+
+		return {
+			group: asistant.group,
+			asistants: asistant.name,
+			students: newfiltered
+		};
+	});
+
+	console.log(asistants);
+	filteredByAssisstantData.value = asistants // assigned this value to ref.
+	return asistants;
+}
+
+// fetchingData() function is imported for readability. Because it is async function, we use await 
+
+onMounted(async() => {
+	const data  = await fecthingData(); 
+	studentsByGroup(data);
+	getGroupNames (data)
+})
+</script>
 <template>
 	<div>
 		<BootcampLogo />
@@ -14,10 +66,12 @@ import HelloWorld from './components/HelloWorld.vue'
 			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
 		</a>
 	</div>
-	<HelloWorld msg="Vite + Vue" />
+<StudentsByGroup :data="filteredByAssisstantData"></StudentsByGroup> 
+
+
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .logo {
 	height: 6em;
 	padding: 1.5em;
@@ -31,4 +85,7 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo.vue:hover {
 	filter: drop-shadow(0 0 2em #42b883aa);
 }
+
+
+
 </style>
---
+++ src/components/StudentsByGroup.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps(["data"]);
+</script>
+<template>
+	<section v-for="data in props.data" ><hr><strong class="group">{{data.group}}</strong> <hr> <br>
+		<strong class="assistan">{{data.asistants}}</strong>  <br> 
+		<div class="student" v-for="data in data.students" >{{data}}</div>
+	</section> 
+</template>
+<style lang="scss" scoped>
+	.assistan {
+	background-color: rgb(57, 126, 153);
+	font-style: normal;
+	border-radius: 5px
+}
+.group {
+	
+	font-style: italic;
+	border-radius: 15px;
+	font-size:170%;
+}
+
+.student {
+	font-style:normal;
+	border-radius: 15px
+}
+	
+	</style>
